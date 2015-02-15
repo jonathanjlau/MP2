b3016782802f45272305f94175279f88217f00f1
jonathanjlau <jonathanjlau@yahoo.com>
2015-02-14 23:06:18 -0600

    step 1

--- coordinator.py
+++ coordinator.py
@@ -1,19 +1,21 @@
-# Tcp Chat server
+# Coordinator server
  
 import socket, select, sys
  
-#Function to broadcast chat messages to all connected clients
+# Function to broadcast chat messages to all connected clients
 def broadcast_data (message):
-    #Do not send the message to master socket //and the client who has send us the message
+    # Broadcast to everyone except the coordinator itself
     for socket in CONNECTION_LIST:
         if socket != server_socket:
             try :
                 socket.send(message)
             except :
-                # broken socket connection may be, chat client pressed ctrl+c for example
+                # broken socket connection i.e. chat client pressed ctrl+c 
                 socket.close()
                 CONNECTION_LIST.remove(socket)
                 
+                
+# Function to find an empty server id for the connecting client             
 def find_empty_id():
     for i in range(1,5):
         key = str(i)
@@ -29,23 +31,25 @@ def find_empty_id():
     
     
 
- 
+# Main function
 if __name__ == "__main__":
      
     # List to keep track of socket descriptors
     CONNECTION_LIST = []
-    RECV_BUFFER = 4096 # Advisable to keep it as an exponent of 2
+    RECV_BUFFER = 4096 
     PORT = 5000
     
-    
+    # Makes sure that the user at least tries to enter a port number.
     if(len(sys.argv) < 2) :
         print 'Usage : python coordinator.py port'
         sys.exit()
     
+    # Assign the port number from the user input
     PORT = int(sys.argv[1])
      
+    # Socket setup
     server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    # this has no effect, why ?
+
     server_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
     server_socket.bind(("0.0.0.0", PORT))
     server_socket.listen(10)
@@ -55,11 +59,14 @@ if __name__ == "__main__":
     CONNECTION_LIST.append(server_socket)
     
     
-    # Hashmaps from id to alphabet or socket and vice versa
-    socket_to_id = { }
+    # Hashmaps from id to respective alphabet letter or socket and vice versa for easier conversion
+    socket_to_id = {}
     id_to_socket = {}
     alpha_to_id = { 'a':'1', 'b':'2', 'c':'3', 'd':'4' }
     id_to_alpha = { '1':'a', '2':'b', '3':'c', '4':'d' }
+    
+    
+    
  
     print "Coordinator server started on port " + str(PORT)
  
@@ -68,50 +75,50 @@ if __name__ == "__main__":
         read_sockets,write_sockets,error_sockets = select.select(CONNECTION_LIST,[],[])
  
         for sock in read_sockets:
-            # New connection
+        
+            # A new connection received through server_socket
             if sock == server_socket:
-                # Handle the case in which there is a new connection recieved through server_socket
+            
                 sockfd, addr = server_socket.accept()
+        
                 
-                
-                
-                
-                #Put the socket data into the two-way hashmap
+                # Put the accepted socket data into the two-way hashmap
                 empty_id = find_empty_id()
                 socket_to_id[sockfd] = empty_id
                 id_to_socket[empty_id] = sockfd
                 CONNECTION_LIST.append(sockfd)
                 
-                
-                
-                
-                
                 print "Server " + socket_to_id[sockfd] +  " (%s, %s) connected" % addr
                  
                 #broadcast_data("[%s:%s] entered room\n" % addr)
+                
+                # Tell the newly connected client what their id is
                 sockfd.sendall("Server " + socket_to_id[sockfd] + "\n")
                 
-                #Tell the client what id they have
+
              
-            #Some incoming message from a client
+             
+            # Message from an existing client
             else:
-                # Data recieved from client, process it
+            
+                # Try to process data recieved from client
                 try:  
                     data = sock.recv(RECV_BUFFER)
                     
-                    print("DATA RECEIVED: " + data) #DEBUGGING
+                    print("DATA RECEIVED: " + data) #FOR DEBUGGING, CAN DELETE
                     
                     request = data.split()
                     
-                    # A direct send request from one server to another, forward only the message
+                    # A direct send request from one server to another, forward the message 
                     if request[0].lower() == "send":
                         sender_id = socket_to_id[sock]
+                        sender_id_alpha = id_to_alpha[sender_id]
                         sender_message = " ".join(request[1:-1])
                         receiver_id = alpha_to_id[request[-1].lower()]
                         receiver_socket = id_to_socket[receiver_id]
-                        receiver_socket.sendall(sender_message + "\n")
+                        receiver_socket.sendall("Message " + " " + sender_id_alpha + " " + sender_message)
                         
-                    # Otherwise send a broadcast
+                    # Otherwise broadcast that message to all the clients
                     else:
                         broadcast_data("\r" + '<' + str(sock.getpeername()) + '> ' + data)                
                  
--- server.py
+++ server.py
@@ -1,11 +1,16 @@
-# telnet program example
+# MP1 server
+
 import socket, select, string, sys
 import Queue 
 import threading
+import datetime
 import time
 import random
 
+# Global variables
+server_id = -1
 
+# Class to store message attributes 
 class Command:
     def __init__(self, message, delay):
         self.message = message
@@ -14,12 +19,48 @@ class Command:
 
  
 
-
+# Simple prompt to remind the client of their id
 def prompt() :
-    sys.stdout.write('<You> ')
+    global server_id
+    sys.stdout.write('< Server ' + server_id + ' / ' + id_to_alpha[server_id] + ' > ')
     sys.stdout.flush()
     
+    
+    
+    
+# A handler for the different options that the user can input via the command line or command file 
+def command_input_handler(data):
+    
+    request = data.split()
+
+
+    # If it is a send request, add it to the delayed message sending queue
+    if (request[0].lower() == "send"):
+
+        receiver_id = alpha_to_id[request[-1].lower()]                  
+        max_delay_to_destination =   max_delay[receiver_id]
+        delay = random.randrange(0, max_delay_to_destination)   
+        command = Command(data,delay)
+        queue_list[int(receiver_id)].put(command)
+        
+        # Output the time that you sent the message according to the spec
+        extracted_message = " ".join(request[1:-1])
+        message_receiver = request[-1].upper()
+        print "Sent \"" + extracted_message + "\" to " + message_receiver + " at system time " + str(datetime.datetime.now())
+
+    
+    # If it is not a valid request, send it to the coordinator to broadcast whatever the message was        
+    else:    
+        coordinator.send(data)
+
+
+        
+        
+        
+# Thread to handle the messages from the coordinator server and user input.    
 def input_receiver() :
+
+    global server_id 
 
     while 1:
          
@@ -28,63 +69,71 @@ def input_receiver() :
          
         for sock in read_sockets:
             # Message from Coordinator server
-            if sock == server:
+            if sock == coordinator:
                 data = sock.recv(4096)
                 if not data :
                     print '\nDisconnected from coordinator'
                     sys.exit()
-                    
+                
+                # Extract the message from the coordinator    
                 request = data.split()
+                
+                # If the message begins with "server", set your new id
                 if request[0].lower() == "server":
 
                     server_id = request[1]
                     print "I am server " + server_id + " / " + id_to_alpha[server_id].upper()
                     prompt()
-                elif request[0].lower() == "caps":
-                    print "CAPS"
+                    
+                # If the message begins with "message", then it is a direct message from a server. Print out the message with the source and timestamp
+                elif request[0].lower() == "message":
+                    source_server = request[1]
+                    direct_message = " ".join(request[2:])
+                    
+                    # Output the time and source that you received the message from according to the spec
+                    print "Received \"" + direct_message + "\" from " + source_server.upper() + " at system time " + str(datetime.datetime.now()) + ". Max delay is " + str(max_delay[str(server_id)]) + " s"
                     prompt()
+                
+                #If it is an unrecognized message, print it out.
                 else :
 
-                    #print data
                     sys.stdout.write(data)
                     prompt()
+ 
              
-            # User issued a command
+            # User issued a command from the command line
             else :          
                         
                 data = sys.stdin.readline()
-                request = data.split()
-                
-                # If it is a send request, add it to the delayed message sending queue
-                if request[0].lower() == "send":
-                    receiver_id = alpha_to_id[request[-1].lower()]
-                    delay = 5
-                    command = Command(data,delay)
-                    queue_list[int(receiver_id)].put(command)
-                    
-                elif request[0].lower() == "quit":
-                    sys.exit()
-                    
-                else:    
-                    server.send(data)
-
+               
+                # Let the handler handle the user input               
+                command_input_handler(data)
+                  
                 prompt()
                 
-
+                
+                
+                
+# Thread to manage sending out messages to the other nodes. There will be four message sending queues, one for every node  
 def message_sender():
-
+    
+    # Create a cache to hold the next messages to be sent out in each respective queue. This is to overcome the fact that Python's queues do not have a peek function.
     cache = ["placeholder", -1, -1, -1, -1]
 
+    # Go through all four message sending queues and send the messages when it is appropriate
     while 1:
         for i in range(1,5):
+            # If the cache is not empty, check if its ready to be sent
             if cache[i] != -1:
                 command = cache[i]
+                # If the delay is up, send the message and empty the respective cache
                 if ((time.time() - command.time) >= command.delay):
-                    server.send(command.message) 
+                    coordinator.send(command.message) 
                     cache[i] = -1
-                    print "MESSAGE SENT: " + command.message
-                    prompt()
                     
+
+                    
+            # If the cache is empty, add the next message to be sent into the cache         
             elif not queue_list[i].empty():
                 cache[i] = queue_list[i].get()
                 
@@ -98,22 +147,50 @@ def message_sender():
                 
                 
  
-#main function
+# Main function
 if __name__ == "__main__":
-     
-    if(len(sys.argv) < 3) :
-        print 'Usage : python server.py hostname port'
-        sys.exit()
-     
-    host = sys.argv[1]
-    port = int(sys.argv[2])
     
-    server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    server.settimeout(2)
+    # Initialize the delay hashmap and input command storage for the input files 
+    max_delay = { '1': 0, '2': 0, '3': 0, '4': 0 }
+    input_commands = []
+
+    # Make sure the user fives at least the port and the config file
+    if ((len(sys.argv) < 3) | (len(sys.argv) > 4)) :
+        print 'Usage : python server.py port config_file [command_file]'
+        sys.exit()
+        
+    # Update the port
+    port = int(sys.argv[1])
+
+    # Add the delay information into the delay hashmap from the config_file
+    if(len(sys.argv) >= 3) :
+        config_file = sys.argv[2]
+        with open(config_file, "r") as ins:
+            i = 1
+            for line in ins:
+                max_delay[str(i)] = int(line)  
+                i = i+1
+
+    # If a command file was given, add those commands into an array
+    if(len(sys.argv) >= 4) :
+        command_file = sys.argv[3]       
+        with open(command_file, "r") as ins:
+            for line in ins:
+                input_commands.append((line))   
+                
+
+    #for i in xrange(1,5):
+    #    print "idx = " + str(i) + " : " + str(max_delay[str(i)])
+
+    # Setup the socket to the coordinator
+    coordinator = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    coordinator.settimeout(2)
      
-    # connect to remote host
+    # Try to connect to the coordinator
+    host = ""
+    
     try :
-        server.connect((host, port))
+        coordinator.connect((host, port))
     except :
         print 'Unable to connect'
         sys.exit()
@@ -121,16 +198,17 @@ if __name__ == "__main__":
     print 'Connected to Coordinator server.'
     
      
-    # Hashmaps from id to alphabet and vice versa
+    # Setup Hashmaps from id to respective alphabet letter and vice versa for easier conversion, similarly to the coordinator
     alpha_to_id = { 'a':'1', 'b':'2', 'c':'3', 'd':'4' }
     id_to_alpha = { '1':'a', '2':'b', '3':'c', '4':'d' }
     
-    # Init stuff
-    server_id = -1
+    # Make a list for all the threads and sockets
     thread_list = []
-    socket_list = [sys.stdin, server]
+    socket_list = [sys.stdin, coordinator]
     
-    queue_list = ["placeholder"]
+    # Make a list of four queues, each of which will represent the message queue for node 1/a, 2/b, 3/c, and 4/d
+    # The placeholder is used here for entry 0 so the code is more elegant and convenient. i.e. The queue for server 1 will be queue_list[1]
+    queue_list = ["placeholder"] 
     for i in xrange(4):
         queue_list.append(Queue.Queue())
     
@@ -145,6 +223,10 @@ if __name__ == "__main__":
     # Start all the threads
     for thread in thread_list:
         thread.start()
+    
+    # If the user had given us a command file, process all those commands with the handler
+    for command in input_commands:
+        command_input_handler(command)
      
     while(1):
         pass
